0c4750d6be2824d5c201b7ae9648df137a169d1e
maxiaoyu <2044580857@qq.com>
2022-11-08 13:08:42 +0800

    lissajous learn🕉

--- lissajous/test/test2.go
+++ lissajous/test/test2.go
@@ -14,8 +14,8 @@ import (
 
 // var palette = []color.Color{color.White, color.RGBA{0x00, 0xff, 0x00, 0xff}}
 
-var c uint16 = 0x93f
-var palette = []color.Color{color.Gray16{c}, color.RGBA{0x00, 0xff, 0x00, 0xff}}
+// var c uint16 = 0x93f
+// var palette = []color.Color{color.Gray16{c}, color.RGBA{0x00, 0xff, 0x00, 0xff}}
 
 const (
 	whiteIndex = 0 // first color in palette
@@ -42,12 +42,15 @@ func lissajous(out io.Writer) {
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
+
+	palette := []color.Color{color.White, color.Black}
 	for i := 0; i < nframes; i++ {
-		// strRand := fmt.Sprintf("%01v", rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(10)+1)
+		// 随机颜色
+		var randUint16 uint16 = uint16(math.Floor(rand.Float64() * 0xffffff))
+		var randUint16_2 uint16 = uint16(math.Floor(rand.Float64() * 0xffffff))
+		// fmt.Printf("输出: %x\n", randUint16)
 
-		// randNum, _ := strconv.ParseFloat(strRand, 10)
-
-		// fmt.Println("输出: ", fmt.Sprintf("%X", math.Floor(randNum*0xffffff)))
+		palette = []color.Color{color.Gray16{randUint16}, color.Gray16{randUint16_2}}
 
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
@@ -55,7 +58,7 @@ func lissajous(out io.Writer) {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				1)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
